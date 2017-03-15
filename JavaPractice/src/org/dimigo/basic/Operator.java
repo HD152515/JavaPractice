63122932113e5f6c2223e43936bb4749d4315025
persi <persi@DESKTOP-S8R1G7T>
2017-03-15 14:53:18 +0900

    디미베네

---
+++ JavaPractice/src/org/dimigo/basic/Operator.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package org.dimigo.basic;
+
+/**
+ * <pre>
+ * org.dimigo.basic
+ * 		|_Operator
+ *
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 3. 14.
+ * </pre>
+ *
+ * @author : 박예원
+ * @version : 1.0
+ */
+public class Operator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int salary = 1700000;
+		int employee = 3;
+		int juampo = 1500;
+		System.out.println("<< 디미베네 연간 인건비 >>");
+		System.out.println("월 평균 급여 : " + String.format("%,d",salary) + "원");
+		System.out.println("점포 내 직원 수 : " + String.format("%,d", employee) + "명");
+		System.out.println("점포 수 : " + String.format("%,d", juampo) + "개");
+		System.out.println("연간 인건비 : " + String.format("%,d", (long)salary * 12 * employee * juampo) + "원");
+	}
+
+}
